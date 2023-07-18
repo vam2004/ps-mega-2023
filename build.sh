3fe63f371e4c675d22d315ac40760138afb007f3
vam2004 <vam2004@github.com>
2023-07-18 00:48:29 -0400

    changed directory structure

--- build.sh
+++ build.sh
@@ -1,11 +1,11 @@
 #! /bin/bash
-
+ROOT_DIR="$(pwd)/build"
 # set classpath
 
 if [ ! -z ${CLASSPATH+x} ]; then
 	echo "[DEBUG] previuos classpath was $CLASSPATH" 
 fi
-JAVA_POSTGRESQL_PATH="$(pwd)/psql-jdbc/"
+JAVA_POSTGRESQL_PATH="$ROOT_DIR/psql-jdbc/"
 if [ -d "$JAVA_POSTGRESQL_PATH" ]; then
 	if [ ! -z ${CLASSPATH+x} ]; then
 		CLASSPATH=$CLASSPATH:$JAVA_POSTGRESQL_PATH
--- install.sh
+++ install.sh
@@ -1,23 +1,32 @@
+ROOT_DIR="$(pwd)/build"
 POSTGRESQL_VERSION=42.6.0
 POSTGRESQL_HASH=b817c67a40c94249fd59d4e686e3327ed0d3d3fae426b20da0f1e75652cfc461
-POSTGRESQL_DIR="$(pwd)/psql-jdbc"
+POSTGRESQL_DIR="$ROOT_DIR/psql-jdbc"
+if [ -d "$ROOT_DIR/downloads" ]; then 
+	echo "[DEBUG] already downloaded"
+	exit
+else
+	mkdir "$ROOT_DIR/downloads" || exit
+	mkdir "$ROOT_DIR/psql-jdbc" || exit
+fi
 download_link() {
 	if [ -f "$3" ]; then
 		echo "[DEBUG] skipping installation of '$3'"
 	else
 		echo "[DEBUG] downloading from '$1'"
-		local tmpfile="./downloads/$2.tmp"
-		echo "[DEBUG] downloading into '$tmpfile'"
+		local tmpfile="$ROOT_DIR/downloads/$2.tmp"
+		echo "[DEBUG] downloading into '$2.tmp'"
 		if [ -f "$tmpfile" ]; then
-			echo "[DEBUG] skipping download: the file '$tmpfile' already exit"
+			echo "[DEBUG] skipping download"
 		else
 			curl "$1" -o "$tmpfile"	--silent
 		fi
-			echo "[DEBUG] expected sha256 '$2'"
+		echo "[DEBUG] verifying download"
 		local hash=$(sha256sum "$tmpfile" | cut -d " " -f1)
 		if [ "$hash" == "$2" ]; then
+			echo "[DEBUG] download verified!"
 			echo "[DEBUG] cloning into '$3'"
-			cp "$tmpfile" "$3"
+			cp "$tmpfile" "$3" || exit
 		else
 			echo "[ERROR] corrupted file found: '$tmpfile'"
 			exit
