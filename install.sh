2c8e9b369a69258ec37b1e43735d8c51d2397e73
vam2004 <vam2004@github.com>
2023-07-26 20:08:35 -0400

    added the file size genarated by 'du'

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 POSTGRESQL_JDBC_VERSION=42.6.0
 POSTGRESQL_JDBC_HASH=b817c67a40c94249fd59d4e686e3327ed0d3d3fae426b20da0f1e75652cfc461
-POSTGRESQL_JDBC_SIZE=0
+POSTGRESQL_JDBC_SIZE=1081604
 # @description: "get size of file" @args: path
 get_size(){
 	return $(du -b postgresql-42.6.0.jar | cut -f1);
