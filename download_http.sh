a6bf6445c839a3a9630a33fe3bf4a4041c6d176d
vam2004 <vam2004@github.com>
2023-08-02 17:12:43 -0400

    compatibility: fixed struing comparation in ./install.sh with dash shell

--- download_http.sh
+++ download_http.sh
@@ -5,12 +5,12 @@ download_http(){
         echo "[ARGS] download_http \$1='$1'"
         echo "[ARGS] download_http \$2='$2'"
         echo "[ARGS] download_http \$3='$3'"
-        if [ "$1" == "continue" ]; then
+        if [ "$1" = "continue" ]; then
                 echo "[EXEC] curl -o '$3' $CURL_DOWNLOAD_OPTIONS -sS '$2'"; # sensible operation
                 curl -o "$3" $CURL_DOWNLOAD_OPTIONS -sS "$2";
                 return $?;
         fi
-        if [ "$1" == "fresh" ]; then
+        if [ "$1" = "fresh" ]; then
                 echo "[EXEC] curl -C - -o '$3' $CURL_DOWNLOAD_OPTIONS -sS '$2'"; # sensible operation
                 curl -C - -o "$3" $CURL_DOWNLOAD_OPTIONS -sS "$2";
                 return $?;
